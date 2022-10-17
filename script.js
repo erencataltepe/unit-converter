398bde340724faf704d3c0ec8538ad3f4a0b7b9a
Eren Çataltepe <erencataltepe@gmail.com>
2022-10-17 17:35:33 +0300

    Hide input arrows.

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("input");
+const convertButton = document.querySelector("button");
+
+convertButton.addEventListener("click", function () {
+  console.log(input.value);
+});
